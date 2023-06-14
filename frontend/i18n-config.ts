a4d03ad12c4f93cfd9b179a6a70af2869d638ce0
Alexandre Tolstenko <tolstenko@gmail.com>
2023-06-14 17:59:04 -0400

    fix: accept only en locale

--- frontend/i18n-config.ts
+++ frontend/i18n-config.ts
@@ -1,6 +1,6 @@
 export const i18n = {
     defaultLocale: 'en',
-    locales: ['en', 'de', 'cs'],
+    locales: ['en'], // todo: add others and use some automatic translation service
 } as const;
 
 export type Locale = typeof i18n['locales'][number];
