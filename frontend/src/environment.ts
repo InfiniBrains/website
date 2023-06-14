296ab66c60b0a4d9d717b6bf7c461db9af8b71d6
Alexandre Tolstenko <tolstenko@gmail.com>
2023-06-14 18:44:16 -0400

    fix: run preseed before starting backend

--- frontend/src/environment.ts
+++ frontend/src/environment.ts
@@ -1,5 +1,5 @@
 export default {
     NEXT_PUBLIC_PAGE_LIMIT: process.env.NEXT_PUBLIC_PAGE_LIMIT || 6,
     NEXT_PUBLIC_STRAPI_FORM_SUBMISSION_TOKEN: process.env.NEXT_PUBLIC_STRAPI_FORM_SUBMISSION_TOKEN || 'your-form-submission-token',
-    NEXT_PUBLIC_STRAPI_API_URL: process.env.NEXT_PUBLIC_STRAPI_API_URL || 'http://127.0.0.1:1337'
+    NEXT_PUBLIC_STRAPI_API_URL: process.env.NEXT_PUBLIC_STRAPI_API_URL || 'https://strapi.infinibrains.com' // 'http://127.0.0.1:1337'
 }
