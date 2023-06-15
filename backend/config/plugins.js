13fa8246b25abb9781637631d96e33ca720039a2
Alexandre Tolstenko <tolstenko@gmail.com>
2023-06-14 20:44:01 -0400

    feat: rollback strapi versions

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -7,24 +7,24 @@ module.exports = ({ env }) => ({
       jwtSecret: env('JWT_SECRET', require('crypto').randomBytes(256).toString('base64')), // crypto.randomBytes(16).toString('base64')),
     }
   },
-  config: {
-    endpoint: '/graphql',
-    shadowCRUD: true,
-    playgroundAlways: false,
-    depthLimit: 7,
-    amountLimit: 100,
-    apolloServer: {
-      tracing: false,
-    },
-  },
-  'random-sort': {
-    enabled: true,
-  },
-  'migrations': {
-    enabled: true,
-    config: {
-      autoStart: true,
-      migrationFolderPath : 'migrations'
-    },
-  },
+  // config: {
+  //   endpoint: '/graphql',
+  //   shadowCRUD: true,
+  //   playgroundAlways: false,
+  //   depthLimit: 7,
+  //   amountLimit: 100,
+  //   apolloServer: {
+  //     tracing: false,
+  //   },
+  // },
+  // 'random-sort': {
+  //   enabled: true,
+  // },
+  // 'migrations': {
+  //   enabled: true,
+  //   config: {
+  //     autoStart: true,
+  //     migrationFolderPath : 'migrations'
+  //   },
+  // },
 });
